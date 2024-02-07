34ae00e147ba48c834b56fc2d44dffb1c65ff8d2
Atanazio01 <marcosd.atanazio@gmail.com>
2024-02-06 21:18:05 -0300

    Creating a ControllerBase

---
+++ src/RocketseatAuction.API/Controllers/OfferController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RocketseatAuction.API.Controllers;
+
+public class OfferController : RocketseatAuctionBaseController
+{
+
+    [HttpPost]
+    public IActionResult CreateOffer()
+    {
+        return Created();
+    }
+}
--- src/RocketseatAuction.API/obj/Debug/net8.0/RocketseatAuction.API.AssemblyInfo.cs
+++ src/RocketseatAuction.API/obj/Debug/net8.0/RocketseatAuction.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RocketseatAuction.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+063cb066886d26e1c296ee24c18c228ea8483b13")]
 [assembly: System.Reflection.AssemblyProductAttribute("RocketseatAuction.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RocketseatAuction.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
