f223a6cb4bf02cc7150e315753f07352e1823cdd
Atanazio01 <marcosd.atanazio@gmail.com>
2024-02-08 14:05:21 -0300

    Unit tests

--- src/RocketseatAuction.API/obj/Debug/net8.0/RocketseatAuction.API.AssemblyInfo.cs
+++ src/RocketseatAuction.API/obj/Debug/net8.0/RocketseatAuction.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RocketseatAuction.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f454c3d240f81edb9edbb3dcda841b2f2c573d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48ff79047c75a55f26ba47d365fe27f28824db92")]
 [assembly: System.Reflection.AssemblyProductAttribute("RocketseatAuction.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RocketseatAuction.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ tests/UseCases.Teste/Auctions/GetCurrent/GetCurrentAuctionUseCaseTest.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Moq;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.Contracts;
+using RocketseatAuction.API.UseCases.Auctions.GetCurrent;
+using Xunit;
+using Bogus;
+using RocketseatAuction.API.Enums;
+
+namespace UseCases.Teste.Auctions.GetCurrent;
+public class GetCurrentAuctionUseCaseTest
+{
+    [Fact]
+    public void Success()
+    {
+        //ARRANGE
+        var entity = new Faker<Auction>()
+            .RuleFor(auction => auction.Id, f => f.Random.Number(1, 700))
+            .RuleFor(auction => auction.Name, f => f.Lorem.Word())
+            .RuleFor(auction => auction.Starts, f => f.Date.Past())
+            .RuleFor(auction => auction.Ends, f => f.Date.Future())
+            .RuleFor(auction => auction.Items, (f, auction) => new List<Item>()
+            {
+                new Item
+                {
+                    Id = f.Random.Number(1, 700),
+                    Name = f.Commerce.ProductName(),
+                    Brand = f.Company.CompanyName(),
+                    Condition = f.PickRandom<Condition>(),
+                    BasePrice = f.Random.Decimal(50, 10000),
+                    AuctionId = auction.Id
+                }
+            }).Generate();
+
+        var mock = new Mock<IAuctionRepository>();
+        mock.Setup(auction => auction.GetCurrent()).Returns(entity);
+
+        var useCase = new GetCurrentAuctionUseCase(mock.Object);
+
+        //ACT
+        var auction = useCase.Execute();
+
+        //ASSERT
+        auction.Should().NotBeNull();
+        auction.Id.Should().Be(entity.Id);
+        auction.Name.Should().Be(entity.Name);
+    }
+}
